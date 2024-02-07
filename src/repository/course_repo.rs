1d5e2bbaa40f8d1948d3797ebfe1cb4e83e52d33
Zhang Yao <cineazhan@gmail.com>
2024-02-07 18:20:13 +0800

    CourseService get_user_course_detail + get_user_courses_tree + get_courses + get_weeks

--- src/repository/course_repo.rs
+++ src/repository/course_repo.rs
@@ -1,6 +1,13 @@
 use std::sync::Arc;
 
-use crate::config::database::Db;
+use async_trait::async_trait;
+use sea_orm::{ColumnTrait, Condition, EntityTrait, QueryFilter};
+
+use crate::{
+    config::database::{DatabaseTrait, Db},
+    entity::course,
+    error::db_error::DbError,
+};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
@@ -14,5 +21,57 @@ impl CourseRepository {
         Self {
             db_conn: Arc::clone(db_conn),
         }
+    }
+}
+
+#[async_trait]
+pub trait CourseRepositoryTrait {
+    async fn get_all_course_detail<T, U>(&self, keys: T) -> Result<U, DbError>
+    where
+        T: IntoIterator<Item = (String, String)> + Send + Sync,
+        U: FromIterator<course::Model> + Send + Sync;
+
+    async fn get_course_detail(
+        &self,
+        key: &(String, String),
+    ) -> Result<Option<course::Model>, DbError>;
+}
+
+#[async_trait]
+impl CourseRepositoryTrait for CourseRepository {
+    async fn get_all_course_detail<T, U>(&self, keys: T) -> Result<U, DbError>
+    where
+        T: IntoIterator<Item = (String, String)> + Send + Sync,
+        U: FromIterator<course::Model> + Send + Sync,
+    {
+        let mut condition = Condition::any();
+
+        for (term, course_no) in keys {
+            let sub_condition = Condition::all()
+                .add(course::Column::CourseTerm.eq(&term))
+                .add(course::Column::CourseNo.eq(&course_no));
+            condition = condition.add(sub_condition);
+        }
+
+        Ok(course::Entity::find()
+            .filter(condition)
+            .all(self.db_conn.get_db())
+            .await?
+            .into_iter()
+            .collect())
+    }
+
+    async fn get_course_detail(
+        &self,
+        key: &(String, String),
+    ) -> Result<Option<course::Model>, DbError> {
+        let (term, course_no) = key;
+        let course = course::Entity::find()
+            .filter(course::Column::CourseTerm.eq(term))
+            .filter(course::Column::CourseNo.eq(course_no))
+            .one(self.db_conn.get_db())
+            .await?;
+
+        Ok(course)
     }
 }
--- src/service/course_service.rs
+++ src/service/course_service.rs
@@ -1,15 +1,22 @@
+use askama::filters::format;
 use async_trait::async_trait;
+use futures::future::ready;
+use futures::{stream, StreamExt};
 use moka::future::{Cache, CacheBuilder};
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QuerySelect};
+use serde_json::to_string;
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
+use std::vec;
 
 use crate::config::database::{DatabaseTrait, Db};
 use crate::config::AppConfig;
 use crate::dto::course_tree::{CourseTree, Week};
 use crate::entity::course::Model;
-use crate::entity::student;
+use crate::entity::{homework, student};
 use crate::error::db_error::DbError;
+use crate::repository::course_repo::{CourseRepository, CourseRepositoryTrait};
 use crate::repository::user_repo::{UserRepository, UserRepositoryTrait};
 use crate::{dto::course_tree, entity::course};
 
@@ -45,9 +52,10 @@ pub trait CourseServiceTrait {
 #[derive(Clone)]
 pub struct CourseService {
     course_cache: Cache<(String, String), course::Model>,
-    week_cache: Cache<(String, String), course_tree::Week>,
+    week_cache: Cache<(String, String), Vec<course_tree::Week>>,
     user_course_cache: Cache<String, Vec<(String, String)>>,
     db_conn: Arc<Db>,
+    course_repo: CourseRepository,
     user_repo: UserRepository,
     app_config: Arc<AppConfig>,
 }
@@ -64,6 +72,7 @@ impl CourseService {
         let user_course_cache = CacheBuilder::new(100)
             .time_to_live(Duration::from_secs(60 * 60 * 24))
             .build();
+        let course_repo = CourseRepository::new(db_conn);
         let user_repo = UserRepository::new(db_conn);
         let app_config = Arc::clone(app_config);
         let db_conn = Arc::clone(db_conn);
@@ -73,6 +82,7 @@ impl CourseService {
             week_cache,
             user_course_cache,
             db_conn,
+            course_repo,
             user_repo,
             app_config,
         }
@@ -92,6 +102,48 @@ impl CourseService {
         }
 
         result
+    }
+
+    async fn get_weeks_loads(&self, term: &str, course_code: &str) -> Result<Vec<Week>, DbError> {
+        // 获取原始数据
+        let data = homework::Entity::find()
+            .filter(homework::Column::HwTerm.eq(term))
+            .filter(homework::Column::HwCourseCode.eq(course_code))
+            .all(self.db_conn.get_db())
+            .await?;
+
+        // 加工数据
+        let week_homework_map: HashMap<_, Vec<_>> =
+            data.into_iter().fold(HashMap::new(), |mut acc, hw| {
+                acc.entry(hw.hw_week).or_insert_with(Vec::new).push(hw);
+                acc
+            });
+
+        // 构造返回数据
+        week_homework_map
+            .into_iter()
+            .map(|(week_index, homework)| {
+                let chapters: Vec<_> = homework
+                    .iter()
+                    .map(|hw| hw.hw_chapter)
+                    .filter(|c| c < &20)
+                    .map(|c| c.to_string())
+                    .collect();
+
+                let mut week_title = format!("第{}周", week_index);
+                if !chapters.is_empty() {
+                    week_title = format!("{} - 第{}章", week_title, chapters.join(","))
+                }
+
+                Week {
+                    number: 1,
+                    homeworks: homework,
+                    content: week_title,
+                }
+            })
+            .collect();
+
+        todo!()
     }
 }
 
@@ -172,19 +224,100 @@ impl CourseServiceTrait for CourseService {
         }
     }
 
-    async fn get_user_course_detail(&self, _user_id: &str) -> Result<Vec<Model>, DbError> {
-        todo!()
+    async fn get_user_course_detail(&self, user_id: &str) -> Result<Vec<Model>, DbError> {
+        let keys = self.get_user_courses(user_id).await?;
+
+        self.course_repo.get_all_course_detail(keys).await
     }
 
-    async fn get_user_courses_tree(&self, _user_id: &str) -> Result<CourseTree, DbError> {
-        todo!()
+    async fn get_courses(&self, keys: &Vec<(String, String)>) -> Result<Vec<Model>, DbError> {
+        let mut result = vec![];
+
+        for key in keys {
+            if !self.course_cache.contains_key(key) {
+                let course = self.course_repo.get_course_detail(key).await?;
+                if course.is_some() {
+                    self.course_cache
+                        .insert(key.clone(), course.as_ref().unwrap().clone())
+                        .await;
+                    result.push(course.unwrap());
+                }
+            } else {
+                let course = self.course_cache.get(key).await.unwrap();
+                result.push(course);
+            }
+        }
+
+        Ok(result)
     }
 
-    async fn get_courses(&self, _keys: &Vec<(String, String)>) -> Result<Vec<Model>, DbError> {
-        todo!()
+    async fn get_user_courses_tree(&self, user_id: &str) -> Result<CourseTree, DbError> {
+        // 获取所有可以访问的课程
+        let course_keys = self.get_user_courses(user_id).await?;
+        // 获取完整的可访问的课程列表
+        let courses = self.get_courses(&course_keys).await?;
+
+        // 将课程按照课程代码分类
+        let course_code_map: HashMap<_, Vec<_>> =
+            courses.into_iter().fold(HashMap::new(), |mut acc, course| {
+                acc.entry((course.course_no.clone(), course.course_code.clone()))
+                    .or_insert_with(Vec::new)
+                    .push(course);
+                acc
+            });
+
+        // 构造树形结构
+        let result = CourseTree {
+            courses: {
+                let courses_stream = stream::iter(course_code_map)
+                    .filter(|(_, v)| ready(!v.is_empty()))
+                    .then(|(_, v)| async move {
+                        let course = course::Model {
+                            course_short_name: "".into(),
+                            course_no: "".into(),
+                            ..v.first().cloned().unwrap()
+                        };
+
+                        let mut course: course_tree::Course = course.into();
+                        course.weeks = self
+                            .get_weeks(&course._course.course_term, &course._course.course_code)
+                            .await?;
+                        Ok(course)
+                    })
+                    .collect::<Vec<Result<course_tree::Course, DbError>>>()
+                    .await
+                    .into_iter()
+                    .collect::<Result<Vec<_>, _>>()?;
+
+                courses_stream
+            },
+        };
+
+        Ok(result)
     }
 
-    async fn get_weeks(&self, _term: &str, _courses_code: &str) -> Result<Vec<Week>, DbError> {
-        todo!()
+    async fn get_weeks(&self, term: &str, courses_code: &str) -> Result<Vec<Week>, DbError> {
+        if self
+            .week_cache
+            .contains_key(&(term.to_string(), courses_code.to_string()))
+        {
+            Ok(self
+                .week_cache
+                .get(&(term.into(), courses_code.into()))
+                .await
+                .unwrap()
+                .clone())
+        } else {
+            let result = self.get_weeks_loads(term, courses_code).await;
+            if result.is_ok() {
+                self.week_cache
+                    .insert(
+                        (term.into(), courses_code.into()),
+                        result.as_ref().unwrap().clone(),
+                    )
+                    .await;
+            }
+            result
+        }
     }
 }
